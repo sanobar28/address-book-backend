0b6f020adef0cdb2afc0870f739c3cb46b8c3080
Namira Mujawar <sanobar.mujawar@gmail.com>
2021-08-17 16:16:15 +0530

    [Sanobar]added: setting dev and prod application properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbookapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,8 +12,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
-		log.info("Address Book App Started");
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book Application Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info("inside main method");
 	}
 
 }
